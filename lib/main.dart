e2bb6b5690358e4a449885cd08acd6b43f923a34
casasmaximiliano <maxiecasas19@gmail.com>
2024-10-16 12:07:56 -0300

    Ejercicio Día 2: Mayor o Menor de Edad

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,12 @@
 void main () {
-  String nombre = "Maximiliano";
-  int edad = 19;
-  double altura = 1.81;
+  int Edad = 100;
 
-  // ignore: avoid_print
-  print ("mi nombre es $nombre, tengo $edad y mido $altura.");
+
+  if (Edad >= 18 && Edad <= 104) {
+    print ("Eres mayor de edad");
+  } else if (Edad >= 105){
+    print ("Ya se te paso la edad");
+  } else {
+    print("Eres menor de edad");
+  }
 }
-
